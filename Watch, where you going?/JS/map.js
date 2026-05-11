efd62435eb1e550f993281303ea99ec080a35483
Giac Quang Do <70329011+DQGiac@users.noreply.github.com>
2026-05-11 15:31:34 +0700

    Update map.js

--- Watch, where you going?/JS/map.js
+++ Watch, where you going?/JS/map.js
@@ -7,318 +7,6 @@ var colliding = function (a, b) {
   );
 };
 
-var levelindex = -1;
-var actuallevel = 0;
-var levelmaps = [
-  // 1 --> introduce time dust
-  [
-    "                              ", //161
-    "                              ", //171
-    "                              ", //181
-    " wwwwwwwwwwwwwwwwwwwwwwwwwwww ", //01
-    "w                            w", //11
-    "w                            w", //21
-    "w                            w", //31
-    "w p                        + w", //41
-    " wwwwwwwwwwwww  wwwwwwwwwwwww ", //51
-    "                              ", //61
-    "                              ", //71
-    "                              ", //81
-    "                              ", //91
-    "                              ", //101
-    "             wwww             ", //111
-    "                              ", //121
-    "        d            d        ", //131
-    "                              ", //141
-    "                              ", //151
-    "                              ", //191
-  ],
-
-  // 2 - introduce button
-  [
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    " p  w   d    w  b ssssss  +",
-    "wwwwwwwwwwwwwwwwwwwwwwwwwww",
-  ],
-  [
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                    ww     ",
-    "    w   d    w  n ssssss  +w",
-    "wwwwwwwwwwwwwwwwwwwwwwwwwww",
-  ],
-  // 3 - more advanced parkour
-  [
-    "                              ", //01
-    "                              ", //11
-    "                              ", //21
-    "                              ", //31
-    "                              ", //41
-    "               wwww   w       ", //51
-    "                              ", //61
-    "                              ", //71
-    "                              ", //81
-    "                              ", //91
-    "                              ", //101
-    "                           +  ", //111
-    "                              ", //121
-    "         w    w               ", //131
-    "       sw                     ", //141
-    "       w                      ", //151
-    "     sw                       ", //161
-    "     w                        ", //171
-    "p   w               b   d     ", //181
-    "wwww               www        ", //191
-  ],
-  [
-    "                              ", //01
-    "                              ", //11
-    "                              ", //21
-    "                              ", //31
-    "                              ", //41
-    "               wwww   w       ", //51
-    "                              ", //61
-    "          w                   ", //71
-    "           w                  ", //81
-    "            w                 ", //91
-    "             w                ", //101
-    "                           +  ", //111
-    "                              ", //121
-    "         w    w               ", //131
-    "       sw                     ", //141
-    "       w                      ", //151
-    "     sw                       ", //161
-    "     w                        ", //171
-    "    w               n   d     ", //181
-    "wwww               www        ", //191
-  ],
-  //  -> medium + sophisticated
-  [
-    "                                        ", //01
-    "                                        ", //11
-    "       b                           p    ", //21
-    "       ww                 w      wwww   ", //31
-    "                          w      w      ", //41
-    "                          w      w   d  ", //51
-    "           w    w     ww  w      w      ", //61
-    "       w                  w      wssssss", //71
-    "   ww                     w      wwwwwww", //81
-    "  ssssssssssssssssssssssssw   b         ", //91
-    "  wwwwwwwwwwwwwwwwwwwwwwwwwwwwww        ", //101
-    "                                w    d  ", //111
-    "                                 w      ", //121
-    "                                  wwwwww",
-    "                 b                      ", //131
-    "ss            w         +               ", //141
-    "ww   ww   ww                            ", //151
-    "                                        ", //161
-    "                         wd             ", //181
-    "                          ww            ", //191
-  ],
-  [
-    "                                        ", //01
-    "                                        ", //11
-    "       b                                ", //21
-    "       ww                 wwwwwwwwwww   ", //31
-    "                          w      w      ", //41
-    "                          w      w   d  ", //51
-    "           w    w     ww  w      w      ", //61
-    "       w                  w      wssssss", //71
-    "   ww                     w      wwwwwww", //81
-    "  ssssssssssssssssssssssssw   n         ", //91
-    "  wwwwwwwwwwwwwwwwwwwwwwwwwwwwww        ", //101
-    "                                w    d  ", //111
-    "                                 w      ", //121
-    "                                  wwwwww",
-    "                 b                      ", //131
-    "ss            w         +               ", //141
-    "ww    w    w                            ", //151
-    "                                        ", //161
-    "                         wd             ", //181
-    "                          ww            ", //191
-  ],
-  [
-    "                                        ", //01
-    "                                        ", //11
-    "       n                                ", //21
-    "       ww                 wwwwwwwwwww   ", //31
-    "                          w      w      ", //41
-    "                          w      w   d  ", //51
-    "           w    w     ww  w      w      ", //61
-    "       w                  w      wssssss", //71
-    "   ww                     w      wwwwwww", //81
-    "  ssssssssssssssssssssssssw   n         ", //91
-    "  wwwwwwwwwwwwwwwwwwwwwwwwwwwwww        ", //101
-    "                                w    d  ", //111
-    "                                 w      ", //121
-    "                                  wwwwww",
-    "                 b                      ", //131
-    "              w         +               ", //141
-    "ww    w    w                            ", //151
-    "                                        ", //161
-    "                         wd             ", //181
-    "                          ww            ", //191
-  ],
-  [
-    "                                        ", //01
-    "                                        ", //11
-    "       n                                ", //21
-    "       ww                 wwwwwwwwwww   ", //31
-    "                          w      w      ", //41
-    "                          w      w   d  ", //51
-    "           w    w     ww  w      w      ", //61
-    "       w                  w      wssssss", //71
-    "   ww                     w      wwwwwww", //81
-    "  ssssssssssssssssssssssssw   n         ", //91
-    "  wwwwwwwwwwwwwwwwwwwwwwwwwwwwww        ", //101
-    "                                w    d  ", //111
-    "                                 w      ", //121
-    "                                  wwwwww",
-    "                 n                      ", //131
-    "              w      s  +               ", //141
-    "ww    w    w      wwwwwww               ", //151
-    "                                        ", //171
-    "                         wd             ", //181
-    "                          ww            ", //191
-  ],
-  // --> could be an intro for laser
-  [
-    "wwwwwwwwwwwwwwwwwwww", //0
-    "55555555555555555555", //1
-    "                    ", //2
-    " p                + ", //3
-    "wwwwwwwwwwwwwww  www", //4
-    "      v    v     w  ", //5
-    "      |    |     w  ", //6
-    "      |    | d   w  ", //7
-    "      ^    ^     w  ", //8
-    "    wwwwwwwwwwwwww  ", //9
-    "          5         ", //10
-    "                    ", //12
-    "                    ", //11
-    "                    ", //12
-    "sss    ss   ss      ", //13
-    "wwwwwwwwwwwwwwww    ", //14
-    "                    ", //15
-    "                  + ", //17
-    "                    ", //18
-    "                    ", //19
-  ],
-
-  //
-  [
-    "                   w", //0
-    "                   w", //1
-    "                   w", //2
-    "p  d               w", //3
-    "wwww               w", //4
-    "   w               w", //5
-    "   w               w", //6
-    "   w               w", //7
-    "   w               w", //8
-    "   w               w", //9
-    "   w               w", //10
-    "   w               w", //11
-    "   w               w", //12
-    "   w               w", //13
-    "   w>-------------<w", //14
-    "                    ", //15
-    "                    ", //16
-    "       ss    ss  +  ", //17
-    "   wwwwwwwwwwwwwwwww", //18
-    "                    ", //19
-  ],
-  // --> harder
-  [
-    "                      wwwwwwww", //0
-    "                       v  v   ", //1
-    "                       |  |   ", //2
-    "                       |  |   ", //3
-    " p                     ^  ^ + ", //4
-    "wwww>----<w           wwwwwwww", //5
-    "   w>----<w                   ", //6
-    "                              ", //7
-    "       ss         w           ", //8
-    "       ww                     ", //9
-    "                              ", //10
-    "                      w       ", //11
-    "                              ", //12
-    "              sss             ", //13
-    "    wwww    wwwwwww           ", //14
-    "                              ", //15
-    "                              ", //16
-    "                              ", //17
-    "                              ", //18
-    "                              ", //19
-  ],
-  
-  
-  
-  
-  
-  
-  
-  
-  // end level
-    [ 
-    "wwwwwwwwwwwwwwwwwwwwwwwwwwwww", //0 
-    "w                           w", //1 
-    "w  >-< w w  w  ww  w w  ww  w", //2 
-    "w   w  w w w+w w w ww  ww   w", //3 
-    "w   w  www www w w w w   w  w", //4 
-    "w   w  w w w w w w w w ww   w", //5 
-    "w                           w", //6 
-    "w             p             w", //7 
-    "w        www  w  ww         w", //8 
-    "w        w   w w w w        w", //9 
-    "w        ww  w w ww         w", //10 
-    "w        w    w  w w        w", //11 
-    "w                           w", //12 
-    "w                           w", //13 
-    "w ww  w    w  w w v ww   ww w", //14 
-    "w w5w w   wsw  ww | w w w   w", //15 
-    "w ww  w   www   w | w w w w w", //16 
-    "w w    ww w w ww  ^ w w  w  w", //17 
-    "w                           w", //18 
-    "wwwwwwwwwwwwwwwwwwwwwwwwwwwww", //19 
-  ], 
-  
-];
-
-var tutorialmaps = [];
-
 // here are the stats for each individual type of block
 var blocks = {
   // wall
@@ -589,7 +277,6 @@ var fillLevel = function (maps, lvl) {
   }
 };
 
-var collidedx = false;
 var collide = function (vx, vy) {
   for (var i in level) {
     var b = level[i];
@@ -606,12 +293,13 @@ var collide = function (vx, vy) {
           if (adding) addon -= p.vx;
           p.vx = 0;
         }
-
+        
         if (vy > 0) {
           p.y = b.y - b.h / 2 - p.h / 2;
           p.vy = 0;
           p.jump = true;
         } else if (vy < 0) {
+          p.jumptimer = 0;
           p.y = b.y + b.h / 2 + p.h / 2;
           p.vy = 0;
         }
@@ -627,8 +315,8 @@ var collide = function (vx, vy) {
           break;
         case "b": //buttons
           if (scene == "game") {
-            levelindex += 1;
-            fillLevel(levelmaps, levelindex);
+            gameindex += 1;
+            fillLevel(gamemaps, gameindex);
           } else if (scene == "tuto") {
             tutoindex += 1;
             fillLevel(tutomaps, tutoindex);
