be7d16e8c5ff60566b78eb22dc46d58941e987c4
Giac Quang Do <70329011+DQGiac@users.noreply.github.com>
2026-05-11 15:28:33 +0700

    Update button-keys.js

--- Watch, where you going?/JS/button-keys.js
+++ Watch, where you going?/JS/button-keys.js
@@ -2,13 +2,9 @@ var add = [0, 0, 0, 0, 0];
 var addtf = [];
 
 function button(x, y, w, h, txt, func, transtf, num) {
-  // rectMode(CENTER);
   imageMode(CENTER);
   textAlign(CENTER, CENTER);
-  // fill(180 - 45 * add[num], 30 * add[num], 0);
-  // rect(x, y + h * 0.7, w, h, 4);
-  // fill(236 - 59 * add[num], 44 * add[num], 0);
-  textSize(40);
+  textSize(25);
   fill(0);
   noStroke();
   if (hover(x - w / 2, x + w / 2, y - h / 2, y + h / 2)) {
@@ -19,35 +15,22 @@ function button(x, y, w, h, txt, func, transtf, num) {
       func();
     }
     image(scene_button_down, x, y + (h * 4) / 32, w, h);
-    text(txt, x, y + (h * 8) / 32 - h / 4);
+    text(txt, x, y + h / 8);
     fill(0, 100);
-    text(txt, x - w / 3, y + (h * 8) / 32 - h / 4);
-    text(txt, x + w / 3, y + (h * 8) / 32 - h / 4);
+    text(txt, x - (w / 24 * txt.length) + (w / 24 * txt.length) * abs(x - width / 2) / width * 2, y + h / 8);
+    text(txt, x + (w / 24 * txt.length) - (w / 24 * txt.length) * abs(x - width / 2) / width * 2, y + h / 8);
   } else {
     image(scene_button_up, x, y, w, (h * 40) / 32);
-    text(txt, x, y - h / 4);
+    text(txt, x, y - h / 8);
     fill(0, 100);
-    text(txt, x - w / 3, y - h / 4);
-    text(txt, x + w / 3, y - h / 4);
+    text(txt, x - (w / 24 * txt.length) + (w / 24 * txt.length) * abs(x - width / 2) / width * 2, y - h / 8);
+    text(txt, x + (w / 24 * txt.length) - (w / 24 * txt.length) * abs(x - width / 2) / width * 2, y - h / 8);
   }
-  // imageMode(CORNER);
 }
 
 var nextscene = "";
 function buttonscene(x, y, w, h, txt, scene, add, num) {
-  button(
-    x,
-    y,
-    w,
-    h,
-    txt,
-    () => {
-      nextscene = scene;
-      add();
-    },
-    true,
-    num
-  );
+  button(x, y, w, h, txt, () => { nextscene = scene; add(); }, true, num);
 }
 
 var hover = (x1, x2, y1, y2) => {
